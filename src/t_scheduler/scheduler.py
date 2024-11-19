331ef497f096d2d7fda36b8696ba82f16e58e18c
jqxcz <jqxczz@gmail.com>
2024-11-19 13:52:49 +1100

    Add gate type support, fix targeting error

--- src/t_scheduler/scheduler.py
+++ src/t_scheduler/scheduler.py
@@ -6,13 +6,14 @@ def debug_gates():
     gate_layers = [[*chain(*(([x] * 8) for x in [5,0,6,8,7]))], ]
     gate_layers2 = [[Gate(t) for t in layer] for layer in gate_layers]
     print(gate_layers)
-    schedule_undermine(20, 5, gate_layers2, True)
+    output = schedule_undermine(20, 5, gate_layers2, True)
+    print(output)
 
 """
 Perform basic scheduler pass
 """
 def schedule_undermine(width, height, gate_layers:list[list["Gate"]], debug: bool=False):
-    board = create_default_board(width, height)
+    board = Widget(width, height)
 
     # global curr_mined_row
     # curr_mined_row = height - 1
@@ -26,57 +27,87 @@ def schedule_undermine(width, height, gate_layers:list[list["Gate"]], debug: boo
             output_layer = []
             new_deferred = []
             for gate in deferred:
-                path = vertical_search(width, height, board, gate.targ*2)
-                if path is None:
+                success = alloc_gate(board, gate)
+                if success:
+                    active.append(gate)
+                else:
                     new_deferred.append(gate)
-                    continue
 
-                active.append(gate)
-                gate.lock = PatchLock(gate, [board[i][j] for i,j in path], 1)
-                gate.lock.lock()
-                output_layer.append(path)
+            for gate in active:
+                output_layer.append(gate.path)
             
             deferred = new_deferred
+
             if debug:
                 print_board(board)
             
+            if not active:
+                raise Exception("No progress made!")
+
             for gate in active:
-                gate.lock.unlock()
+                gate.tick()
+                if gate.retirable():
+                    gate.retire()
+
             active = []
             output_layers.append(output_layer)
 
         output_layer = []
         for gate in layer:
-            path = vertical_search(width, height, board, gate.targ*2)
-            if path is None:
+            success = alloc_gate(board, gate)
+            if success:
+                active.append(gate)
+            else:
                 deferred.append(gate)
-                continue
-            
-            print(gate.targ, path)
-            active.append(gate)
-            gate.lock = PatchLock(gate, [board[i][j] for i,j in path], 1)
-            gate.lock.lock()
-            output_layer.append(path)
+
 
         # print(deferred)
 
         if debug:
             print_board(board)
-        
+
         for gate in active:
-            gate.lock.unlock()
+            output_layer.append(gate.path)
+
+        for gate in active:
+            gate.tick()
+            if gate.retirable():
+                gate.retire()
         active = []
         output_layers.append(output_layer)
 
 
     return output_layers
-        
+
+
+def alloc_gate(widget, gate):
+    if gate.gate_type == GateType.T_STATE:
+        path = vertical_search(widget, gate.targ)
+        if path is None:
+            return False
+        path[0].use()
+
+        gate.activate(path, path[0])
+        return True
+    elif gate.gate_type == GateType.NO_RESOURCE:
+        reg = widget[0, gate.targ*2]
+        if reg.locked():
+            return False
+        gate.activate([reg])
+        return True
+    elif gate.gate_type == GateType.ANCILLA:
+        reg = widget[0, gate.targ*2]
+        anc = widget[1, gate.targ*2]
+        if reg.locked() or anc.locked():
+            return False
+        gate.activate([reg, anc])
+        return True
 
 def col_search_order(start_col, width):
     return chain(range(start_col - 1, 0, -1), range(start_col + 1, width - 1))
 
-def vertical_search(*args):
-    return fix_left_col(vertical_search_main(*args))
+# def vertical_search(*args):
+#     return fix_left_col(vertical_search_main(*args))
 
 def fix_left_col(path):
     if path is None:
@@ -86,70 +117,154 @@ def fix_left_col(path):
 
 
 
-def vertical_search_main(width, height, board: list[list["Patch"]], start_col):
-    if board[0][start_col].locked() or board[0][start_col + 1].locked():
+def T_search_owning(widget, reg) -> Patch | None:
+    for r in range(2, widget.height):
+        for c in range(2 * reg, 2 * reg + 2):
+            if (patch := widget[r,c]).T_available():
+                return patch
+
+
+
+def T_path_owning(widget, reg, T_patch):
+    if 2 * reg == T_patch.col:
+        return [widget[r, T_patch.col] for r in range(T_patch.row, -1,-1)]
+    else:
+        hor = [widget[1, 2*reg], widget[0, 2*reg]]
+        vert =  [widget[r, T_patch.col] for r in range(T_patch.row, 0,-1)]
+        return vert + hor
+
+
+def probe_left_nonowning(widget, reg, prefix):
+    start_row = prefix[-1].row
+    start_col = prefix[-1].col
+
+    left_path = []
+
+    if start_row > 1:
+        for c in range(start_col-1, 0, -1):
+            if (patch := widget[start_row,c]).route_available():
+                left_path.append(patch)
+            elif patch.T_available():
+                return [patch] + left_path[::-1] + prefix[::-1]
+            else:
+                break
+
+    next_left_path = []
+    for r in range(start_row - 1, 1, -1):
+        for c in range(start_col - 1, 0, -1):
+            if (patch := widget[r,c]).route_available():
+                next_left_path.append(patch)
+            elif patch.T_available() and patch.col >= left_path[-1].col:
+                # breakpoint()
+                return [patch] + left_path[:start_col - c][::-1] + prefix[:r+2][::-1]
+            else:
+                break
+        left_path = next_left_path
+
+def probe_right_nonowning(widget, reg, prefix):
+    start_row = prefix[-1].row
+    start_col = prefix[-1].col
+
+    right_path = []
+
+    if start_row > 1:
+        for c in range(start_col+1, widget.width - 1):
+            if (patch := widget[start_row,c]).route_available():
+                right_path.append(patch)
+            elif patch.T_available():
+                return [patch] + right_path[::-1] + prefix[::-1]
+            else:
+                break
+
+    next_right_path = []
+    for r in range(start_row - 1, 1, -1):
+        for c in range(start_col+1, widget.width-1):
+            if (patch := widget[r,c]).route_available():
+                next_right_path.append(patch)
+            elif patch.T_available() and patch.col <= right_path[-1].col:
+                return [patch] + right_path[c - start_col :-1:-1] + prefix[r::-1]
+            else:
+                break
+        right_path = next_right_path
+
+# def T_search_nonowning(widget, reg):
+#     for r in range(widget.height-1,1,-1):
+#         # left col of reg -> left edge
+#         for c in range(reg * 2-1,0,-1):
+#             if (patch := widget[r,c]).T_available():
+#                 return patch
+#         for c in range(reg * 2+2,widget.width):
+#             if (patch := widget[r,c]).T_available():
+#                 return patch
+
+# def T_path_nonowning(widget, reg, T_patch):
+#     if T_patch.row == widget.height - 1:
+#         # T on left of reg
+#         if T_patch.col < 2 * reg:
+#             down_path = [widget[r, 2 * reg] for r in range(widget.height - 1, -1, -1)]
+#             left_path = [widget[T_patch.row, c] for c in range(T_patch.col, 2 * reg)]
+#             return left_path + down_path
+#         else:
+#             down_path = [widget[r, 2 * reg] for r in range(widget.height - 1, -1, -1)]
+#             right_path = [widget[T_patch.row, c] for c in range(T_patch.col, 2 * reg, -1)]
+#             return right_path + down_path
+#     else:
+#         if T_patch.col < 2 * reg:
+#             down_path = [widget[r, 2 * reg] for r in range(T_patch.row + 1, -1, -1)]
+#             left_path = [widget[T_patch.row + 1, c] for c in range(T_patch.col, 2 * reg)]
+#             target = [T_patch]
+#             return target + left_path + down_path
+#         else:
+#             down_path = [widget[r, 2 * reg] for r in range(T_patch.row + 1, -1, -1)]
+#             left_path = [widget[T_patch.row + 1, c] for c in range(T_patch.col, 2 * reg, -1)]
+#             target = [T_patch]
+#             return target + left_path + down_path
+        
+def probe_down(widget, reg):
+    if widget[0, 2*reg].locked() or widget[0, 2*reg + 1].locked():
+        return []
+    prefix = [widget[0, 2 * reg], widget[1, 2 * reg]]
+    probe_col = 2 * reg
+    if reg == 0:
+        prefix.append(widget[1, 1])
+        probe_col = 1
+    for i in range(2, widget.height):
+        if (patch := widget[i, probe_col]).route_available():
+            prefix.append(patch)
+        else:
+            break
+    return prefix
+
+# T --> reg
+def validate_T_path(path):
+    # print(path)
+    if not path[0].T_available():
+        return False
+    if not all(p.route_available() for p in path[1:-1]):
+        return False
+    if path[-1].locked():
+        return False
+    return True
+
+def vertical_search(widget, reg):
+    if widget[0,2*reg].locked():
         return None
-    # down_search
-    for row in range(1, height):
-        if board[row][start_col].locked():
-            # Blocked
-            break
-        elif board[row][start_col].T_available():
-            # Use T in column
-            board[row][start_col].use()
-            return [(r, start_col) for r in range(row, -1, -1)]
-        elif board[row][start_col + 1].T_available():
-            board[row][start_col + 1].use()
-            return [(r, start_col + 1) for r in range(row, 0, -1)] + \
-                [(1, start_col + 1), (1, start_col + 0), (0, start_col)]
 
-
-    # mine sideways (left) 
-    for col in range(start_col - 1, 0, -1):
-        if board[height - 1][col].T_available():
-            down_path = [(r, start_col) for r in range(height - 1, -1, -1)]
-            left_path = [(row, c) for c in range(col, start_col)]
-            board[height - 1][col].use()
-            return left_path + down_path
-        elif board[height - 1][col].locked():
-            break
-
-    # mine sideways (right)
-    for col in range(start_col + 1, width - 1):
-        if board[height - 1][col].T_available():
-            down_path = [(r, start_col) for r in range(height - 1, -1, -1)]
-            right_path = [(row, c) for c in range(col, start_col, -1)]
-            board[height - 1][col].use()
-            return right_path + down_path
-        elif board[height - 1][col].locked():
-            break
-
-    # mine sideways and up 1 (left) then (right)
-    for row in range(height - 2, 1, -1):
-        for col in range(start_col - 1, 0, -1):
-            if board[row][col].T_available():
-                # Try route
-                down_path = [(r, start_col) for r in range(row + 1, -1, -1)]
-                left_path = [(row + 1, c) for c in range(col, start_col)]
-                target = [(row, col)]
-                if any(board[r][c].locked() for r, c in chain(target, left_path, down_path)):
-                    # Path blocked
-                    break
-                board[row][col].use()
-                return target + left_path + down_path
- 
-        for col in range(start_col + 1, width - 1):
-            if board[row][col].T_available():
-                # Try route
-                down_path = [(r, start_col) for r in range(row + 1, -1, -1)]
-                left_path = [(row + 1, c) for c in range(col, start_col, -1)]
-                target = [(row, col)]
-                if any(board[r][c].locked() for r,c in chain(target, left_path, down_path)):
-                    # Path blocked
-                    break
-                board[row][col].use()
-                return target + left_path + down_path
+    if (T := T_search_owning(widget, reg)) and validate_T_path(path := T_path_owning(widget, reg, T)):
+        return path
     
+    if not (prefix := probe_down(widget, reg)):
+        # breakpoint()
+        return None
+
+    # breakpoint()
+
+    if path := probe_left_nonowning(widget, reg, prefix):
+        return path
+
+    if path := probe_right_nonowning(widget, reg, prefix):
+        return path
+
     return None
 
 
--- src/t_scheduler/util.py
+++ src/t_scheduler/util.py
@@ -1,12 +1,44 @@
 from enum import Enum
 from abc import ABC
+from itertools import cycle, islice
 
 
+class GateType(Enum):
+    NO_RESOURCE = 1
+    ANCILLA = 2
+    T_STATE = 3
 
 class Gate():
-    def __init__(self, targ):
+    def __init__(self, targ, gate_type = GateType.T_STATE, duration = 1):
         self.targ: int = targ
         self.lock: None | PatchLock = None
+        self.gate_type = gate_type
+        self.duration = duration
+        self.timer = 0
+        self.resource = None
+
+    def tick(self):
+        self.timer += 1
+    
+    def retirable(self):
+        return self.timer >= self.duration
+    
+    def activate(self, path, resource = None):
+        self.path = path
+        self.lock = PatchLock(self, path, self.duration)
+        if resource:
+            resource.use()
+            self.resource = resource
+        self.lock.lock()
+    
+    def retire(self):
+        if self.resource:
+            self.resource.release()
+        assert self.lock is not None
+        self.lock.unlock()
+        
+
+
 
 class PatchType(Enum):
     REG = 1
@@ -36,11 +68,20 @@ class Patch:
     def T_available(self):
         return self.patch_type == PatchType.T and not self.used
     
+    def route_available(self):
+        return self.patch_type == PatchType.ROUTE and not self.locked()
+
     def use(self):
         if self.patch_type == PatchType.T:
             self.used = True
         else:
             raise Exception("Can't use non-T!")
+        
+    def release(self):
+        if self.patch_type != PatchType.T or not (self.used):
+            raise Exception("Can't release non-T or unused T!")
+        self.used = False
+        self.patch_type = PatchType.ROUTE
 
 class PatchLock:
     def __init__(self, owner: "Gate", holds: list[Patch], duration: int):
@@ -64,14 +105,35 @@ class Route:
         self.path = [(patch.row, patch.col) for patch in patches]
 
 
-def create_default_board(w: int, h: int):
-    # row-major (row, col)
-    reg_row = [Patch(PatchType.REG, 0, c) for c in range(w)]
-    route_row = [Patch(PatchType.ROUTE, 1, c) for c in range(w)]
-    board = [reg_row, route_row]
-    for r in range(2, h):
-        row = [Patch(PatchType.BELL, r, 0), 
-               *(Patch(PatchType.T, r, c) for c in range(1, w-1)),
-               Patch(PatchType.BELL, r, w-1)]
-        board.append(row)
-    return board
+class Widget:
+    def __init__(self, width, height):
+        self.width: int = width
+        self.height: int = height
+
+        reg_row = [Patch(PatchType.REG, 0, c) for c in range(width)]
+        route_row = [Patch(PatchType.ROUTE, 1, c) for c in range(width)]
+        board = [reg_row, route_row]
+        for r in range(2, height):
+            row = [Patch(PatchType.BELL, r, 0), 
+                *(Patch(PatchType.T, r, c) for c in range(1, width-1)),
+                Patch(PatchType.BELL, r, width-1)]
+            board.append(row)
+        self.board = board
+
+    def __getitem__(self, index):
+        if isinstance(index, tuple):
+            return self.board[index[0]][index[1]]
+        return self.board[index]
+    
+
+
+
+
+def roundrobin(*iterables):
+    "Visit input iterables in a cycle until each is exhausted."
+    # roundrobin('ABC', 'D', 'EF') → A D E B F C
+    # Algorithm credited to George Sakkis
+    iterators = map(iter, iterables)
+    for num_active in range(len(iterables), 0, -1):
+        iterators = cycle(islice(iterators, num_active))
+        yield from map(next, iterators)
