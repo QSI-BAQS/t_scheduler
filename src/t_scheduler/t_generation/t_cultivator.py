a30cede90d1633e388b0c378729ee4884e37b14a
Alan Robertson <alan@qrop.xyz>
2024-12-09 10:35:32 +1100

    Debugging T cultivation

---
+++ src/t_scheduler/t_generation/t_cultivator.py
@@ -0,0 +1,57 @@
+from abc import ABC
+from t_generator import TGenerator
+
+class TCultivator(TGenerator):
+    '''
+        Base T generator object
+    '''
+    STAGE_CYCLES = 0
+    STAGE_PROB = 1
+    stages = [
+        [4, 0.7],  # Injection
+        [5, 0.25],  # Cultivation
+        [5, 0.3],  # Escape
+        [10, 1 / 6] # Gap   
+    ]
+
+    def __init__(self,
+        generator = None  # RNG  
+    ):
+        super().__init__(
+            n_cycles = 22,
+            height = 1,
+            width = 1,
+            n_emitted = 1,
+            generator = generator
+        )
+        self.curr_stage = 0
+        self.n_stages = 4
+
+    def __call__(self) -> int:
+        '''
+            Calls the update method and emits a number of $T$ states
+        '''
+        return self.update()
+
+    def update(self, prob: bool = True):
+        '''
+            Update the state of the generator 
+            :: prob : bool :: Are emissions probablistic   
+            Returns the number of T states produced that cycle
+        '''
+        if self._curr_cycle == self.stages[self.curr_stage][self.STAGE_CYCLES]:
+            self._curr_cycle = 0
+            if prob and self._generator.random() < self.stages[self.curr_stage][self.STAGE_PROB]: # Go to next stage
+                self.curr_stage += 1
+                if self.curr_stage == self.n_stages: 
+                    self.curr_stage = 0
+                    return self.n_emitted
+            elif not prob:
+                if self.curr_stage == self.n_stages: 
+                    self.curr_stage = 0
+                    return self.n_emitted
+
+                return self.n_emitted 
+            return 0
+        self._curr_cycle += 1
+        return 0
---
+++ src/t_scheduler/t_generation/t_generator.py
@@ -0,0 +1,49 @@
+from abc import ABC
+import numpy as np
+
+class TGenerator(ABC):
+    '''
+        Base T generator object
+    '''
+    def __init__(self,
+        n_cycles: int,  # Number of cycles to emit
+        height: int,  # Height of block
+        width: int,  # Width of block
+        n_emitted: int = 1,  # Number of T states per emission  
+        prob: float = 1,  # Probability of generation  
+        generator = None  # RNG  
+    ):
+
+        if generator is None:
+            generator = np.random.default_rng()
+
+        self.n_cycles = n_cycles
+        self.height = height
+        self.width = width
+        self.n_emitted = n_emitted
+        self.prob = prob
+        self._generator = generator
+
+        self._curr_cycle = 0  # Current cycle 
+
+    def __call__(self) -> int:
+        '''
+            Calls the update method and emits a number of $T$ states
+        '''
+        return self.update()
+
+    def update(self, prob: bool = True):
+        '''
+            Update the state of the generator 
+            :: prob : bool :: Are emissions probablistic   
+            Returns the number of T states produced that cycle
+        '''
+        if self._curr_cycle == self.n_cycles:
+            self.n_cycles = 0
+            if prob and self._generator.random() < prob:
+                return self.n_emitted
+            elif not prob:
+                return self.n_emitted 
+            return 0
+        self.n_cycles += 1
+        return 0
