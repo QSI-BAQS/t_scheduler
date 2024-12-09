3869d6341cd279e6f121a321b500059bf6a5ce56
jqxcz <jqxczz@gmail.com>
2024-12-09 11:06:50 +1100

    Initial work on flat scheduler

--- src/t_scheduler/flat_scheduler.py
+++ src/t_scheduler/flat_scheduler.py
@@ -1,6 +1,12 @@
+from ast import parse
+from collections import deque
+from multiprocessing import active_children
 from typing import List
 from t_scheduler import gate
-from t_scheduler.gate import BaseGate, T_Gate
+from t_scheduler.gate import BaseGate, GateType, T_Gate
+from t_scheduler.patch import Patch, PatchOrientation, PatchType
+from t_scheduler.scheduler import RotationStrategy, print_board
+from t_scheduler.widget import Widget
 
 
 def toffoli_example_input():
@@ -113,6 +119,168 @@ def dag_prune(dag_layers: List[List[BaseGate]], gates: List[BaseGate]):
 obj = toffoli_example_input()
 x, y = dag_create(obj)
 
+def topological_sort(dag_roots):
+    seen = set()
+    stack = []
+    L = []
+    for root in dag_roots:
+        if root in seen:
+            continue
+        stack.append((root, 0))
+        while stack:
+            node, child_idx = stack.pop()
+            if child_idx == 0 and node in seen:
+                continue
+            elif child_idx >= len(node.post):
+                seen.add(node)
+                L.append(node)
+                continue
+            stack.append((node, child_idx + 1))
+            stack.append((node.post[child_idx], 0))
+    return L
 
-def parse_weights():
-    pass
+
+def parse_weights(gate_layers):
+    parse_order = topological_sort(gate_layers[0])
+
+    # debug!
+    # for g in gates:
+    #     g.schedule_weight = None
+
+    for gate in parse_order:
+        if len(gate.post) == 0:
+            gate.schedule_weight = 0
+        else:
+            gate.schedule_weight = sum(g.schedule_weight + g.weight for g in gate.post)
+
+    
+    # for g in gates:
+    #     print(g, g.schedule_weight)
+
+
+
+class FlatScheduler:
+    def __init__(
+        self,
+        gate_layers,
+        widget,
+        # rot_strat: RotationStrategy,
+        debug: bool = False,
+    ):
+        # self.rot_strat = rot_strat
+        self.widget = widget
+
+        self.waiting = deque(gate_layers)
+        self.queued = deque()
+        self.deferred = []
+        self.next_deferred = []
+        self.active = []
+        self.next_active = []
+
+        self.output_layers = []
+
+        self.curr_layer = []
+
+        self.debug = debug
+
+        self.ROTATION_DURATION = 3
+        self.time = 0
+
+        self.hazard = {}
+
+    def schedule(self):
+        # while self.waiting:
+        self.queued.extend(self.waiting.popleft())
+
+        while self.queued or self.deferred or self.active:
+            self.schedule_pass()
+
+    def schedule_pass(self):
+        # breakpoint()
+        for gate in self.deferred:
+            if gate.available() and self.alloc_gate(gate):
+                pass
+            else:
+                self.next_deferred.append(gate)
+
+        self.deferred = self.next_deferred
+        self.next_deferred = []
+
+        next_queued = []
+        for gate in self.queued:
+            if gate.available() and self.alloc_gate(gate):
+                pass
+            else:
+                next_queued.append(gate)
+
+        self.queued = next_queued
+
+        if self.debug:
+            print_board(self.widget)
+
+        if not self.active:
+            raise Exception("No progress!")
+
+        output_layer = []
+        for gate in self.active:
+            output_layer.append(gate.path)
+        self.output_layers.append(output_layer)
+
+        for gate in self.active:
+            gate.tick()
+
+        for gate in self.active:
+            gate.cleanup(self)
+
+        for gate in self.active:
+            # gate.next(self)
+            if not gate.completed():
+                self.next_active.append(gate)
+            else:
+                for child in gate.post:
+                    if all(g.completed() for g in child.pre):
+                        self.queued.append(child)
+        self.active = self.next_active
+        self.next_active = []
+
+        self.time += 1
+
+    def alloc_gate(self, gate):
+        path = [self.widget[0, gate.targ * 2]]
+        if any(p.locked() for p in path): 
+            return False
+        gate.activate(path, Patch(PatchType.T, -1, -1))
+        self.active.append(gate)
+        return True
+    
+    # def alloc_gate(self, gate):
+    #     if gate.gate_type == GateType.T_STATE:
+    #         path = self.search(self.widget, gate.targ)
+    #         # TODO unhack
+    #         if path is None and not self.widget[0, 2 * gate.targ].locked():
+    #             self.hazard[self.time] = self.hazard.get(self.time, 0) + 1
+    #         if path is None:
+    #             return False
+    #         return self.process_rotation(path, gate)
+    #     else:
+    #         raise NotImplementedError()
+
+    # def process_rotation(self, path, gate):
+
+    #     T_patch = path[0]
+    #     attack_patch = path[1]
+
+    #     matching_rotation = (T_patch.row == attack_patch.row) ^ (
+    #         T_patch.orientation == PatchOrientation.Z_TOP
+    #     )
+
+    #     if matching_rotation:
+    #         T_patch.use()
+    #         gate.activate(path, path[0])
+    #         self.active.append(gate)
+    #         return True
+    #     else:
+    #         return False
+
+wid = Widget.default_widget(obj['n_qubits'] * 2, 3)
+z = FlatScheduler(x, wid, True)
