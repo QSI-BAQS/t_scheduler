9144387c5faee7b6fdd271d103b67565f8b88065
Alan Robertson <alan@qrop.xyz>
2025-01-09 15:38:57 +1100

    Base fully connected router

---
+++ src/t_scheduler/router/fully_connected_router.py
@@ -0,0 +1,16 @@
+from ..router.transaction import Transaction
+from ..widget.route_bus import RouteBus
+
+class FullyConnectedRouter:
+    '''
+        Fully connected router for Ion traps networks
+    '''
+
+    def __init__(self, ancillae_queue, factory_queue, bell_queue) -> None:
+        self.ancillae_queue = ancillae_queue
+        self.t_factory_queue = factory_queue
+        self.bell_queue = bell_queue
+
+    def request_transaction(self, transaction_type): 
+
+        return Transaction(path, [])
